acdc5399c255cc00735f8c119b143a76a4b607e9
mmfiber <masa.03191996@gmail.com>
2019-11-23 19:00:13 +0900

    remove unnecessary folder

--- vagrant/ubuntu64_18/workspace/my-first-shell/mk-template-dir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "テンプレートディレクトリのディレクトリ名を入力してください。"
-read -p "ディレクトリ名： " dname
-mkdir $dname
-cd $dname
-touch "1"
-touch "2"
-touch "3"
--- vagrant/ubuntu64_18/workspace/my-first-shell/quiz.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "日本で二番目に高い山は槍ヶ岳でしょうか？ [y/n]" input
-if [ $input = "n" ]; then
-	echo 正解です。
-else 
-	echo 不正解です。
-fi
-
